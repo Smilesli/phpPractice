53ce05ac98bed0d3657a7e98edaf20068c12a035
Yueli Zhu <yueli.zhu@canada.ca>
2021-06-04 11:52:05 -0400

    Connect to multi databases

--- project_remote/includes/database.php
+++ project_remote/includes/database.php
@@ -4,10 +4,20 @@ $dbHost = "operations.hc-sc.gc.ca";
 $dbUser = "yuzhu";
 $dbPass = "uhk2FZ*uB4jC";
 $dbName_apm = "apm";
+$dbName_sam = "sam";
 //Connetion to database
-$conn = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName_apm);
+$conn_apm = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName_apm);
+$conn_sam = mysqli_connect($dbHost,$dbUser,$dbPass,$dbName_sam);
 
-if($conn) {   
+if($conn_apm) {   
+    mysql_select_db($dbName_apm,$conn_apm)
+}
+else{
+    die("Database connection failed");
+}
+
+if($conn_sam) {   
+    mysql_select_db($dbName_sam,$conn_sam)
 }
 else{
     die("Database connection failed");
