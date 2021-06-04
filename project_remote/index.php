1a219127cfc178be65d078572c732585a725c28f
Yueli Zhu <yueli.zhu@canada.ca>
2021-06-04 14:35:18 -0400

    Publish data into the table

--- project_remote/index.php
+++ project_remote/index.php
@@ -9,23 +9,32 @@ require_once 'includes/database.php';
     <title>Document</title>
 </head>
 <body>
+
+<table>
+   <tr>
+     <th>Name</th>
+
+   </tr>
 <?php
-$sql = "SELECT name FROM packages";
+$sql_apm = "SELECT name FROM packages";
 
 $sql_sam = "SELECT Package FROM cost_recovery";
 
 // $sql = "SELECT sam.cost_recovery.Package, apm.packages.name FROM sam.cost_recovery join apm.packages on sam.cost_recovery.`App ID` = apm.packages.app_id";
-// $result = mysqli_query($conn_apm, $sql);
+$result = mysqli_query($conn_apm, $sql_apm);
 
-$result = mysqli_query($conn_sam, $sql_sam);
+// $result = mysqli_query($conn_sam, $sql_sam);
 $rowCount = mysqli_num_rows($result);
 
 if ($rowCount>0){
-    while($row = mysqli_fetch_assoc($result)){
-        echo $row['Package']."<br>";
-        // echo $row['name']."<br>";
+    while($row = mysqli_fetch_assoc($result)){?>
+        <!-- // echo $row['Package']."<br>";
+        // echo $row['name']."<br>"; -->
+        <tr>
+            <td><?php echo $row['name']; ?></td>
+        </tr>
     
-     }
+<?php     }
 }
 
 else {
